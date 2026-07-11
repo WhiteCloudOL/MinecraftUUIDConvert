a87420d4d7b6c5173169171f008e8ab503701a11
WhiteCloudCN <113251172@qq.com>
2026-07-11 23:38:12 +0800

    chore: set version to 0.1.1

--- src/minecraft_uuid_convert/__init__.py
+++ src/minecraft_uuid_convert/__init__.py
@@ -4,4 +4,4 @@ from minecraft_uuid_convert.converter import ConversionMode, ConversionResult
 from minecraft_uuid_convert.uuid_tools import offline_uuid
 
 __all__ = ["ConversionMode", "ConversionResult", "offline_uuid"]
-__version__ = "1.0.0"
+__version__ = "0.1.1"
