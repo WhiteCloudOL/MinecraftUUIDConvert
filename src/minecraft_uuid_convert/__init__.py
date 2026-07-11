21d2e7982460be6f6ca93d59dcbe3238855ce210
WhiteCloudCN <113251172@qq.com>
2026-07-11 23:41:43 +0800

    chore: bump version to 0.1.2

--- src/minecraft_uuid_convert/__init__.py
+++ src/minecraft_uuid_convert/__init__.py
@@ -4,4 +4,4 @@ from minecraft_uuid_convert.converter import ConversionMode, ConversionResult
 from minecraft_uuid_convert.uuid_tools import offline_uuid
 
 __all__ = ["ConversionMode", "ConversionResult", "offline_uuid"]
-__version__ = "0.1.1"
+__version__ = "0.1.2"
